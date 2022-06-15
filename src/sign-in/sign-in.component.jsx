b4395d5daed9ea03751f9d5a190e41308effbe9a
Hossen Ali <hossen11@cse.pstu.ac.bd>
2022-06-15 20:39:29 +0200

    sign-up  with google

--- src/sign-in/sign-in.component.jsx
+++ src/sign-in/sign-in.component.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { signInWithPopup } from "firebase/auth";
+import React from "react";
+import { createUserDocumentFromAuth, signInWithGooglePopup } from "../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-    return ( <>
-    <h1>Hi,I am from sign-in......</h1>
-    </> );
-}
- 
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    //console.log("Hi",response);
+    const userDocref = createUserDocumentFromAuth(response.user);
+  };
+
+  return (
+    <>
+      <h1>Hi,I am from sign-in......</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </>
+  );
+};
+
 export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,54 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAvcE7EQozwOP1H9CGwzmIPNNt-8STT8cE",
+    authDomain: "my-ecommerce-b0dd8.firebaseapp.com",
+    projectId: "my-ecommerce-b0dd8",
+    storageBucket: "my-ecommerce-b0dd8.appspot.com",
+    messagingSenderId: "576378124915",
+    appId: "1:576378124915:web:9cb4dd769dfa09518e8806"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account",
+})
+
+export const auth = getAuth();
+
+//sign up ..................
+export const signInWithGooglePopup = () => (
+    signInWithPopup(auth, provider)
+);
+
+//saving user to firebase database
+const db = getFirestore(); //creating instance of firebase database
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    console.log(userDocRef);
+
+    const userSnapshot = await getDoc(userDocRef);
+    //console.log(userSnapshot);
+     console.log(userSnapshot.exists());
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createAt = new Date();
+
+        try {
+            await setDoc(userDocRef,{displayName,email,createAt});
+        } catch (error) {
+            console.log("error from ", error.message);
+        }
+    }
+    return userDocRef;
+
+}
+

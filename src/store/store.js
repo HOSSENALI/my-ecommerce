22b98a1b62d41b0b16eaa2eff1bd4a6e868a31fd
Hossen Ali <hossen11@cse.pstu.ac.bd>
2022-08-02 11:32:11 +0200

    adding redux persist to save the data into local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
@@ -8,17 +10,31 @@ const loggerMiddleware = (store) => (next) => (action) => {
     return next(action);
   }
 
-  // console.log('type: ', action.type);
-  // console.log('payload: ', action.payload);
-  // console.log('currentState: ', store.getState());
+  console.log('type: ', action.type);
+  console.log('payload: ', action.payload);
+  console.log('currentState: ', store.getState());
 
   next(action);
 
-  // console.log('next state: ', store.getState());
+  console.log('next state: ', store.getState());
 };
 
 const middleWares = [loggerMiddleware];
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
